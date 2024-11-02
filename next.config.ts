77482a91c3341ecb11330ed3943b419e7733beae
Heppy Andriawan <heppyandriawan@gmail.com>
2024-11-02 15:45:52 +0800

    02 November 2024
    Fixing page reload on page scroll in peoduction

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repo = isGithubActions ? '/FlyingBird' : '';
-const outputDir = isGithubActions? 'export' : undefined;
-const path = process.env.NODE_ENV === "development"?  '' : repo;
-
-console.log(path);
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: outputDir,
-  basePath: `${path}`,
-  assetPrefix: `${path}`,
+  output: 'export',
+  basePath: `/FlyingBird`,
+  assetPrefix: `/FlyingBird`,
   images: {
     remotePatterns: [
       {
